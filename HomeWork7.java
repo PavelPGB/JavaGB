859e0347012b253717ad11e72280ffd36e03cbfb
Pavel Pulyk <93239926+PavelPGB@users.noreply.github.com>
2021-12-01 22:36:54 +0300

    Add files via upload

--- HomeWork7.java
+++ HomeWork7.java
@@ -2,20 +2,30 @@
 * Java 1. HomeWork - 7
 *
 * @author Pavel Pulyk
-* @version 25.11.2021
+* @version 0.0.2 01.12.2021
 */
 
 class HomeWork7 {
     public static void main(String[] args) {
-        Cat[] cats = {new Cat("Timon", 8), new Cat("Arti", 3), new Cat("Murchik", 2)};
-        Plate plate = new Plate(10, 5);
+        Cat[] cats = {new Cat("Timon", 15), new Cat("Arti", 10), new Cat("Murchik", 20)};
+        Plate plate = new Plate(50, 30);
+        System.out.println(plate);
         
         for (Cat cat : cats) {
             cat.eat(plate);
-            plate.addFood(3);
             System.out.println(cat);
-            System.out.println(plate);
         }
+
+            System.out.println(plate);
+            plate.addFood(40);
+            System.out.println(plate);
+
+        for (Cat cat : cats) {
+            cat.setSatiety(false);
+            cat.eat(plate);
+            System.out.println(cat);  
+        }
+        System.out.println(plate);
     }
 }
 
@@ -27,9 +37,19 @@ class Cat {
     Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
-        this.satiety = satiety; 
+        satiety = false; 
     }
  
+    void setSatiety(boolean status) {
+        satiety = status;
+    }
+    
+    void eat(Plate plate) {
+        if (!satiety)
+            satiety = plate.decreaseFood(appetite);
+    }
+
+    /* еще один ваиант реализации метода eat
     void eat(Plate plate) { 
         if (!satiety) {
             satiety = false;
@@ -37,8 +57,8 @@ class Cat {
         if (satiety = plate.decreaseFood(appetite)) {
             satiety = true;
         }
-    }
-
+    }*/
+    
     @Override
     public String toString() {
         return "name: " + name + ", appetite: " + appetite + ", satiety: " + satiety;
@@ -49,23 +69,30 @@ class Plate {
     protected int food;   
     protected int additive;
 
-    Plate(int food , int additive) { 
-        this.food = food; 
+    Plate(int additive, int food) { 
         this.additive = additive; 
+        this.food = food;   
     }
  
-    void addFood(int food) {  
-        if (food <= additive)               
-            food += food;
+    boolean decreaseFood(int portion) {
+        if (food < portion) return false;
+        food -= portion;
+        return true;
     }
 
-    boolean decreaseFood(int n) {  
+    /* еще один вариант реализации метода decreaseFood
+    boolean decreaseFood(int n) { 
         if ((food - n) < 0) {  
             return false;
         }
             food -= n; {    
             return true;
         }    
+    }*/
+
+    void addFood(int food) {
+        if (this.food + food <= additive)
+            this.food += food;
     }
 
     @Override
